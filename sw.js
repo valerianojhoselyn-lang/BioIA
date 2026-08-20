237c60ed1be373090605cdd9f00ba59ba88ddf96
valerianojhoselyn-lang <valerianojhoselyn@gmail.com>
2026-08-20 09:16:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,26 @@
-const CACHE_NAME = "bioia-2.3-v2";
-const CORE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-512.png"
-];
+const CACHE_NAME = "bioia-2.4-v1";
+const CORE = ["./","./index.html","./manifest.json","./icon-512.png"];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)));
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request))
   );
 });
